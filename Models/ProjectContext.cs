08b47d6920b279d0f7bc73b81898e325434c667a
bo.johnson <bo@letterx.co>
2018-06-22 13:08:43 -0700

    Added Projects API Model

---
+++ Models/ProjectContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Music_Matchup.Models
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
