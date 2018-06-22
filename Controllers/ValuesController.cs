64544cae5686264095d601ef8eb6f20b89e5603f
bo.johnson <bo@letterx.co>
2018-06-22 13:06:45 -0700

    Added generic API and connection info

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace Music_Matchup.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
