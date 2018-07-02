8b3235616d23f7e4e57ebf6cb8f8ae3b363936ef
bo.johnson <bo@letterx.co>
2018-07-01 21:40:58 -0700

    Added data validation to registration fields API

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace Music_Matchup.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public string Username { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
